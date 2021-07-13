67ba45e283f9319ea3d26ea6da85da6062a6e576
Arjan <arjanvanschendel@gmail.com>
2021-07-13 21:08:22 +0200

    fix menu?

--- js/menu.js
+++ js/menu.js
@@ -8,19 +8,14 @@ var menu =  '<nav class="navbar navbar-expand-lg navbar-light bg-light">' +
                     '<li class="nav-item active">' +
                         '<a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>' +
                     '</li>' +
-                    '<li class="nav-item dropdown">' +
-                        '<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">' +
-                            'CS:GO' +
-                        '</a>' +
-                        '<li class="nav-item">' +
-                            '<a class="nav-link" href="bindgenerator.html">Bind generator</a>' +
-                        '</li>' +
-                        '<li class="nav-item">' +
-                            '<a class="nav-link" href="stratroulette.html">StratRoulette</a>' +
-                        '</li>' +
+                    '<li class="nav-item">' +
+                        '<a class="nav-link" href="bindgenerator.html">Bind generator</a>' +
                     '</li>' +
                     '<li class="nav-item">' +
-                        '<a class="nav-link" href="videos.html">Important video</a>' +
+                        '<a class="nav-link" href="stratroulette.html">StratRoulette</a>' +
+                    '</li>' +
+                    '<li class="nav-item">' +
+                    '<a class="nav-link" href="videos.html">Important video</a>' +
                     '</li>' +
                 '</ul>' +
                 '</div>' +
